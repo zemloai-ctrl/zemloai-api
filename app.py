5bb872d1e1595cbdcd65d5edcba9b28d0c714610
zemloai-ctrl <info.zemloai@gmail.com>
2026-02-14 16:44:41 +0200

    Update app.py

--- app.py
+++ app.py
@@ -91,6 +91,4 @@ def get_stats():
         "server_type": "Starter-7EUR"
     })
 if __name__ == "__main__":
-    import os
-    port = int(os.environ.get("PORT", 10000))
-    app.run(host="0.0.0.0", port=port)
+    app.run()
